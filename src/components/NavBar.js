197ce2ee04c7f254103b1431ae3cc90ff4b1587a
sophiallen <smlelevich@gmail.com>
2016-09-02 19:03:17 -0700

    now switches buttons based on logged in state

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,16 @@ var Link = require('react-router').Link;
 
 module.exports = React.createClass({
 	render: function(){
+		var LoginBtn;
+		var regBtn;
+		if (this.props.loggedIn){
+			LoginBtn = <li><Link to="/logout"><button className="btn btn-primary">Log Out</button></Link></li>;
+			regBtn = null;
+		} else {
+			LoginBtn = <li><Link to="/login"><button className="btn btn-primary">Log In</button></Link></li>;
+			regBtn = <li><Link to="/register"><button className="btn btn-primary">Register</button></Link></li>;
+
+		}
 		return (
 			<nav className="navbar navbar-inverse navbar-fixed-top">
 				<div className="navbar-header">
@@ -14,11 +24,11 @@ module.exports = React.createClass({
 					<ul className="nav navbar-nav">
 						<li><Link to="/">Home</Link></li>
 						<li><Link to="/about">About</Link></li>
-						<li><Link to="/login">Login</Link></li>
+						<li><Link to="/dashboard">Dashboard</Link></li>
 					</ul>
 					<ul className="nav navbar-nav navbar-right">
-						<li><Link to="/register"><button className="btn btn-primary">Register</button></Link></li>
-						<li><Link to="/login"><button className="btn btn-primary">Log In</button></Link></li>
+						{regBtn}
+						{LoginBtn}
 					</ul>
 				</div>
 			</nav>);
